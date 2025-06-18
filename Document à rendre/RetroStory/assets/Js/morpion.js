351e25077a13b4faed7dc28153bb9831bde0f7de
Cboukhal <boukhalfa.camil@hotmail.fr>
2025-06-18 10:30:47 +0200

    Update morpion.js

--- Document à rendre/RetroStory/assets/Js/morpion.js
+++ Document à rendre/RetroStory/assets/Js/morpion.js
@@ -76,7 +76,7 @@ function symbole(){
                     return;
                 jouer(i,j,sym);
                 fini = game_over();
-                if(egalite()==true)
+                if(egalite()==true && fini == false)
                 {
                     document.getElementById("message").textContent = "Aucun joueur n'a gagné ! Vous êtes à égalité";
                     return;
